f54b8a42e1a26014dd66bc2f2a26c3c436a25da5
Intanari <119494557+Intanari@users.noreply.github.com>
2023-03-04 16:24:35 +0900

    Add files via upload

---
+++ tugas pemrograman 2.php
@@ -0,0 +1,5 @@
+<?php
+$Angka = "40";
+
+$Ang = $Angka % 2 == 0 ? "bilangan genap" : "bilangan ganjil";
+ echo $Ang;
---
+++ tugas pemrograman.php
@@ -0,0 +1,20 @@
+<?php
+$Sepatu ="Puma";
+
+switch($buah) {
+    case "Vans":
+        echo "Merk Sepatu vans dengan harga Rp  1.000.000", PHP_EOL;
+    break;
+    case "NIke":
+        echo "Merk Sepatu Nike dengan harga Rp 450.000 ", PHP_EOL;
+    break;
+    case "Adidas":
+        echo "Merk Sepatu Adidas dengan harga Rp 1.500.000", PHP_EOL;
+    break;
+    case "New Balance":
+        echo " Merk Sepatu New Balance dengan harga 4.000.000 ", PHP_EOL;
+    break;
+    default:
+        echo " Sepatu Converse dengan harga Rp 2.000.000", PHP_EOL;
+    break;
+}
